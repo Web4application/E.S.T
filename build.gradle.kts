06ba2aa9841246aa174d146213127d9e746c531d
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-02-07 09:59:35 +0100

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+dependencies {
+    implementation("io.ktor:ktor-server-core:2.3.0")
+    implementation("io.ktor:ktor-server-netty:2.3.0")
+    implementation("ch.qos.logback:logback-classic:1.4.7")
+}
